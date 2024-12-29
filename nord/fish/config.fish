18c76434fd42a90315b6857159ca5ff097ef7568
zistro <zistro@proton.me>
2024-12-29 20:50:18 +0600

    Add files via upload

---
+++ nord/fish/config.fish
@@ -0,0 +1,9 @@
+colorscript -e 36
+#neofetch
+oh-my-posh init fish --config ~/.config/fish/nordc.omp.json | source
+set -g fish_greeting ""
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
